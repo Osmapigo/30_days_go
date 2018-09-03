c3a86a1d0658feb37465a32a86b21604e9373022
Oscar Piragauta <opiragauta@clickdelivery.com>
2018-09-03 17:43:36 -0500

    d8

--- Day_7/day_7.go
+++ Day_7/day_7.go
@@ -1,7 +1,35 @@
+/*
+Objective
+Today, we're learning about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Input Format
+
+The first line contains an integer,  (the size of our array).
+The second line contains  space-separated integers describing array 's elements.
+
+Constraints
+
+, where  is the  integer in the array.
+Output Format
+
+Print the elements of array  in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+*/
 package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"strconv"
@@ -13,17 +41,15 @@ func main() {
 
 	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
 	checkError(err)
-	n := int32(nTemp)
+	n := int(nTemp)
 
 	arrTemp := strings.Split(readLine(reader), " ")
 
-	var arr []int32
-
-	for i := 0; i < int(n); i++ {
-		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+	for i := 1; i <= n; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[n-i], 10, 64)
 		checkError(err)
 		arrItem := int32(arrItemTemp)
-		arr = append(arr, arrItem)
+		fmt.Print(arrItem, " ")
 	}
 }
 
