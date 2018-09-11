3c0eb4b38841f86b94c7394ebf6cc6f8a83423dd
Oscar Piragauta <oscarl1718@gmail.com>
2018-09-10 23:36:08 -0500

    Day 8 really late

--- Day_8/day_8.go
+++ Day_8/day_8.go
@@ -5,5 +5,35 @@ import (
 )
 
 func main() {
-	fmt.Print("hola")
+	var entries int
+	fmt.Scan(&entries)
+	directory := setDirectory(entries)
+	searchNames(entries, directory)
+}
+
+func setDirectory(entries int) map[string]int {
+	var directory = make(map[string]int)
+	var name string
+	var number int
+
+	for i := 0; i < entries; i++ {
+		fmt.Scan(&name, &number)
+		directory[name] = number
+	}
+	return directory
+}
+
+func searchNames(entries int, directory map[string]int) {
+	var name string
+	for i := 0; i < entries; i++ {
+		n, _ := fmt.Scanln(&name)
+		if n == 0 {
+			break
+		}
+		if directory[name] != 0 {
+			fmt.Printf("%v=%v\n", name, directory[name])
+		} else {
+			fmt.Println("Not found")
+		}
+	}
 }
